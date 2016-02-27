bf683dfc1f171f657eb3ee917dacf438b9affd45
wlm81 <wlman@caida.co.uk>
2016-02-27 00:55:21 +0000

    Added files via upload

--- cacheSolve.r
+++ cacheSolve.r
@@ -1,12 +1,12 @@
-cacheSolve <- function(){
+cacheSolve <- function(x,...){
         i <- x$getinverse()
         if(!is.null(i)) {
                 message("getting cached data")
                 return(i)
         }
-        data <- x$get()
+        data <- matrix(x$get(),nrow=2,ncol=2,byrow=TRUE)
         i <- solve(data, ...)
-        x$setinverse(m)
+        x$setinverse()
         i
 }
 
--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,13 +1,12 @@
 makeCacheMatrix <- function(x = numeric()) {
         i <- NULL
-		m <- matrix(x,nrow=2,ncol=2,byrow=TRUE)
-		print(m)
+        m <<- matrix(x,nrow=2,ncol=2,byrow=TRUE)
         set <- function(y) {
                 x <<- y
                 i <<- NULL
         }
-		get <- function() x
-        setinverse <- function(solve) m <<- solve
+        get <- function() x
+        setinverse <- function(){ i <<- solve(m)}
         getinverse <- function() i
         list(set = set, get = get,
              setinverse = setinverse,
